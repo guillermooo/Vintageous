df595b37039d209a02251a82305265cf6b1bab26
guillermooo <guilan70@hotmail.com>
2012-12-29 19:30:58 +0100

    clean up

--- vintageous_actions.py
+++ vintageous_actions.py
@@ -8,273 +8,273 @@ from Vintageous.vi.constants import MODE_NORMAL
 
 
 class ViEditAtEol(sublime_plugin.TextCommand):
-	def run(self, edit, extend=False):
-		state = VintageState(self.view)
-		state.enter_insert_mode()
+    def run(self, edit, extend=False):
+        state = VintageState(self.view)
+        state.enter_insert_mode()
 
-		self.view.run_command('collapse_to_direction')
+        self.view.run_command('collapse_to_direction')
 
-		sels = list(self.view.sel())
-		self.view.sel().clear()
+        sels = list(self.view.sel())
+        self.view.sel().clear()
 
-		new_sels = []
-		for s in sels:
-			hard_eol = self.view.line(s.b).end()
-			new_sels.append(sublime.Region(hard_eol, hard_eol))
+        new_sels = []
+        for s in sels:
+            hard_eol = self.view.line(s.b).end()
+            new_sels.append(sublime.Region(hard_eol, hard_eol))
 
-		for s in new_sels:
-			self.view.sel().add(s)
+        for s in new_sels:
+            self.view.sel().add(s)
 
 
 class ViEditAfterCaret(sublime_plugin.TextCommand):
-	def run(self, edit, extend=False):
-		state = VintageState(self.view)
-		state.enter_insert_mode()
+    def run(self, edit, extend=False):
+        state = VintageState(self.view)
+        state.enter_insert_mode()
 
-		visual = self.view.has_non_empty_selection_region()
+        visual = self.view.has_non_empty_selection_region()
 
-		sels = list(self.view.sel())
-		self.view.sel().clear()
+        sels = list(self.view.sel())
+        self.view.sel().clear()
 
-		new_sels = []
-		for s in sels:
-			if visual:
-				new_sels.append(sublime.Region(s.end(), s.end()))
-			else:
-				if not utils.is_at_eol(self.view, s):
-					new_sels.append(sublime.Region(s.end() + 1, s.end() + 1))
-				else:
-					new_sels.append(sublime.Region(s.end(), s.end()))
+        new_sels = []
+        for s in sels:
+            if visual:
+                new_sels.append(sublime.Region(s.end(), s.end()))
+            else:
+                if not utils.is_at_eol(self.view, s):
+                    new_sels.append(sublime.Region(s.end() + 1, s.end() + 1))
+                else:
+                    new_sels.append(sublime.Region(s.end(), s.end()))
 
-		for s in new_sels:
-			self.view.sel().add(s)
+        for s in new_sels:
+            self.view.sel().add(s)
 
 
 class ViPaste(sublime_plugin.TextCommand):
-	def run(self, edit, register=None, count=1):
-		# text = sublime.get_clipboard()
-		state = VintageState(self.view)
+    def run(self, edit, register=None, count=1):
+        # text = sublime.get_clipboard()
+        state = VintageState(self.view)
 
-		if register:
-			text = state.registers[register]
-		else:
-			# TODO: There should be a simpler way of getting the unnamed register's content.
-			text = state.registers['"']
+        if register:
+            text = state.registers[register]
+        else:
+            # TODO: There should be a simpler way of getting the unnamed register's content.
+            text = state.registers['"']
 
-		if text.endswith('\n'):
-			if not text.startswith('\n'):
-				text = '\n' + text
-			text = text[0:-1]
+        if text.endswith('\n'):
+            if not text.startswith('\n'):
+                text = '\n' + text
+            text = text[0:-1]
 
-		sels = list(self.view.sel())
+        sels = list(self.view.sel())
 
-		for s in sels:
-			if text.startswith('\n'):
-				if utils.is_at_eol(self.view, s) or utils.is_at_bol(self.view, s):
-					self.paste_all(edit, self.view.line(s.b).b, text, count)
-				else:
-					self.paste_all(edit, self.view.line(s.b - 1).b, text, count)
-			else:
-				self.paste_all(edit, s.b + 1, text, count)
+        for s in sels:
+            if text.startswith('\n'):
+                if utils.is_at_eol(self.view, s) or utils.is_at_bol(self.view, s):
+                    self.paste_all(edit, self.view.line(s.b).b, text, count)
+                else:
+                    self.paste_all(edit, self.view.line(s.b - 1).b, text, count)
+            else:
+                self.paste_all(edit, s.b + 1, text, count)
 
-	def paste_all(self, edit, at, text, count):
-		for x in range(count):
-			self.view.insert(edit, at, text)
+    def paste_all(self, edit, at, text, count):
+        for x in range(count):
+            self.view.insert(edit, at, text)
 
 
 class ViPasteBefore(sublime_plugin.TextCommand):
-	def run(self, edit, register=None, count=1):
-		# text = sublime.get_clipboard()
-		state = VintageState(self.view)
+    def run(self, edit, register=None, count=1):
+        # text = sublime.get_clipboard()
+        state = VintageState(self.view)
 
-		if register:
-			text = state.registers[register]
-		else:
-			# TODO: There should be a simpler way of getting the unnamed register's content.
-			text = state.registers['"']
+        if register:
+            text = state.registers[register]
+        else:
+            # TODO: There should be a simpler way of getting the unnamed register's content.
+            text = state.registers['"']
 
-		if text.endswith('\n'):
-			if not text.startswith('\n'):
-				text = '\n' + text
-			text = text[0:-1]
+        if text.endswith('\n'):
+            if not text.startswith('\n'):
+                text = '\n' + text
+            text = text[0:-1]
 
-		sels = list(self.view.sel())
+        sels = list(self.view.sel())
 
-		for s in sels:
-			if text.startswith('\n'):
-				if not utils.is_at_bol(self.view, s):
-					self.paste_all(edit, self.view.line(s.b - 1).a - 1, text, count)
-				else:
-					self.paste_all(edit, s.b - 1, text, count)
-			else:
-				self.paste_all(edit, s.b, text, count)
+        for s in sels:
+            if text.startswith('\n'):
+                if not utils.is_at_bol(self.view, s):
+                    self.paste_all(edit, self.view.line(s.b - 1).a - 1, text, count)
+                else:
+                    self.paste_all(edit, s.b - 1, text, count)
+            else:
+                self.paste_all(edit, s.b, text, count)
 
-	def paste_all(self, edit, at, text, count):
-		for x in range(count):
-			self.view.insert(edit, at, text)
+    def paste_all(self, edit, at, text, count):
+        for x in range(count):
+            self.view.insert(edit, at, text)
 
 
 class ViEnterNormalMode(sublime_plugin.TextCommand):
-	def run(self, edit):
-		state = VintageState(self.view)
-		self.view.run_command('collapse_to_direction')
-		self.view.run_command('dont_stay_on_eol_backward')
-		state.enter_normal_mode()
+    def run(self, edit):
+        state = VintageState(self.view)
+        self.view.run_command('collapse_to_direction')
+        self.view.run_command('dont_stay_on_eol_backward')
+        state.enter_normal_mode()
 
 
 class ViEnterNormalModeFromInsertMode(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sels = list(self.view.sel())
-		self.view.sel().clear()
+    def run(self, edit):
+        sels = list(self.view.sel())
+        self.view.sel().clear()
 
-		new_sels = []
-		for s in sels:
-			if s.a <= s.b:
-				if (self.view.line(s.a).a != s.a):
-					new_sels.append(sublime.Region(s.a - 1, s.a - 1))
-				else:
-					new_sels.append(sublime.Region(s.a, s.a))
-			else:
-				new_sels.append(s)
+        new_sels = []
+        for s in sels:
+            if s.a <= s.b:
+                if (self.view.line(s.a).a != s.a):
+                    new_sels.append(sublime.Region(s.a - 1, s.a - 1))
+                else:
+                    new_sels.append(sublime.Region(s.a, s.a))
+            else:
+                new_sels.append(s)
 
-		for s in new_sels:
-			self.view.sel().add(s)
+        for s in new_sels:
+            self.view.sel().add(s)
 
-		state = VintageState(self.view)
-		state.enter_normal_mode()
+        state = VintageState(self.view)
+        state.enter_normal_mode()
 
 
 class ViEnterInsertMode(sublime_plugin.TextCommand):
-	def run(self, edit):
-		state = VintageState(self.view)
-		state.enter_insert_mode()
-		self.view.run_command('collapse_to_direction')
+    def run(self, edit):
+        state = VintageState(self.view)
+        state.enter_insert_mode()
+        self.view.run_command('collapse_to_direction')
 
 
 class ViEnterVisualMode(sublime_plugin.TextCommand):
-	def run(self, edit):
-		state = VintageState(self.view)
-		state.enter_visual_mode()
-		self.view.run_command('extend_to_minimal_width')
+    def run(self, edit):
+        state = VintageState(self.view)
+        state.enter_visual_mode()
+        self.view.run_command('extend_to_minimal_width')
 
 
 class ViEnterVisualLineMode(sublime_plugin.TextCommand):
-	def run(self, edit):
-		state = VintageState(self.view)
-		state.enter_visual_line_mode()
+    def run(self, edit):
+        state = VintageState(self.view)
+        state.enter_visual_line_mode()
 
 
 class SetAction(IrreversibleTextCommand):
-	def __init__(self, view):
-		IrreversibleTextCommand.__init__(self, view)
+    def __init__(self, view):
+        IrreversibleTextCommand.__init__(self, view)
 
-	def run(self, action):
-		state = VintageState(self.view)
-		state.action = action
-		state.run()
+    def run(self, action):
+        state = VintageState(self.view)
+        state.action = action
+        state.run()
 
 
 class SetMotion(IrreversibleTextCommand):
-	def __init__(self, view):
-		IrreversibleTextCommand.__init__(self, view)
+    def __init__(self, view):
+        IrreversibleTextCommand.__init__(self, view)
 
-	def run(self, motion):
-		state = VintageState(self.view)
-		state.motion = motion
-		state.run()
+    def run(self, motion):
+        state = VintageState(self.view)
+        state.motion = motion
+        state.run()
 
 
 class SetComposite(sublime_plugin.TextCommand):
-	def run(self, edit, motion, action):
-		state = VintageState(self.view)
-		state.motion = motion
-		state.action = action
-		state.run()
+    def run(self, edit, motion, action):
+        state = VintageState(self.view)
+        state.motion = motion
+        state.action = action
+        state.run()
 
 
 class ViPushDigit(sublime_plugin.TextCommand):
-	def run(self, edit, digit):
-		state = VintageState(self.view)
-		if not (state.action or state.motion):
-			state.push_motion_digit(digit)
-		elif state.action:
-			state.push_action_digit(digit)
+    def run(self, edit, digit):
+        state = VintageState(self.view)
+        if not (state.action or state.motion):
+            state.push_motion_digit(digit)
+        elif state.action:
+            state.push_action_digit(digit)
 
 
 class ViReverseCaret(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sels = list(self.view.sel())
-		self.view.sel().clear()
+    def run(self, edit):
+        sels = list(self.view.sel())
+        self.view.sel().clear()
 
-		new_sels = []
-		for s in sels:
-			new_sels.append(sublime.Region(s.b, s.a))
+        new_sels = []
+        for s in sels:
+            new_sels.append(sublime.Region(s.b, s.a))
 
-		for s in new_sels:
-			self.view.sel().add(s)
+        for s in new_sels:
+            self.view.sel().add(s)
 
 
 class ViEnterNormalInsertMode(sublime_plugin.TextCommand):
-	def run(self, edit):
-		state = VintageState(self.view)
-		state.enter_normal_insert_mode()
+    def run(self, edit):
+        state = VintageState(self.view)
+        state.enter_normal_insert_mode()
 
-		self.view.run_command('toggle_record_macro')
-		# ...User types text...
+        self.view.run_command('toggle_record_macro')
+        # ...User types text...
 
 
 class ViRunNormalInsertModeActions(sublime_plugin.TextCommand):
-	def run(self, edit):
-		state = VintageState(self.view)
-		# We've recorded what the user has typed into the buffer. Turn macro recording off.
-		self.view.run_command('toggle_record_macro')
+    def run(self, edit):
+        state = VintageState(self.view)
+        # We've recorded what the user has typed into the buffer. Turn macro recording off.
+        self.view.run_command('toggle_record_macro')
 
-		for i in range(state.count - 1):
-			self.view.run_command('run_macro')
+        for i in range(state.count - 1):
+            self.view.run_command('run_macro')
 
-		# Ensure the count will be deleted.
-		state.mode = MODE_NORMAL
-		# Delete the count now.
-		state.reset()
+        # Ensure the count will be deleted.
+        state.mode = MODE_NORMAL
+        # Delete the count now.
+        state.reset()
 
-		self.view.run_command('vi_enter_normal_mode_from_insert_mode')
+        self.view.run_command('vi_enter_normal_mode_from_insert_mode')
 
 
 class SetRegister(sublime_plugin.TextCommand):
-	def run(self, edit, character=None):
-		state = VintageState(self.view)
-		if character is None:
-			state.expecting_register = True
-		else:
-			state.register = character
-			state.expecting_register = False
+    def run(self, edit, character=None):
+        state = VintageState(self.view)
+        if character is None:
+            state.expecting_register = True
+        else:
+            state.register = character
+            state.expecting_register = False
 
 
 class ViF(sublime_plugin.TextCommand):
-	def run(self, edit, character=None):
-		state = VintageState(self.view)
-		if character is None:
-			state.motion = 'vi_f'
-			state.expecting_user_input = True
-		else:
-			state.user_input = character
-			state.expecting_user_input= False
-			state.run()
+    def run(self, edit, character=None):
+        state = VintageState(self.view)
+        if character is None:
+            state.motion = 'vi_f'
+            state.expecting_user_input = True
+        else:
+            state.user_input = character
+            state.expecting_user_input= False
+            state.run()
 
 
 class ViT(IrreversibleTextCommand):
-	def __init__(self, view):
-		IrreversibleTextCommand.__init__(self, view)
+    def __init__(self, view):
+        IrreversibleTextCommand.__init__(self, view)
 
-	def run(self, character=None):
-		state = VintageState(self.view)
-		if character is None:
-			state.motion = 'vi_t'
-			# XXX: Maybe we should simply use ["t", "<character>"] in the key map and be done
-			# with this.
-			state.expecting_user_input = True
-		else:
-			state.user_input = character
-			state.expecting_user_input= False
-			state.run()
+    def run(self, character=None):
+        state = VintageState(self.view)
+        if character is None:
+            state.motion = 'vi_t'
+            # XXX: Maybe we should simply use ["t", "<character>"] in the key map and be done
+            # with this.
+            state.expecting_user_input = True
+        else:
+            state.user_input = character
+            state.expecting_user_input= False
+            state.run()
 
