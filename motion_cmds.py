158bf170070b7541d2311f90e3b8f74251e9beea
guillermooo <guilan70@hotmail.com>
2013-01-06 06:46:07 +0100

    improve t and T commands

--- motion_cmds.py
+++ motion_cmds.py
@@ -26,7 +26,8 @@ class ViMoveToHardBol(sublime_plugin.TextCommand):
         for s in new_sels:
             self.view.sel().add(s)
 
-
+# FIXME: Only find exact char counts. Vim ignores the command when the count is larger than the
+# number of instances of the sought character.
 class ViFindInLineInclusive(sublime_plugin.TextCommand):
     def run(self, edit, extend=False, character=None, _internal_mode=None, count=1):
         def f(view, s):
@@ -60,6 +61,8 @@ class ViFindInLineInclusive(sublime_plugin.TextCommand):
         regions_transformer(self.view, f)
 
 
+# FIXME: Only find exact char counts. Vim ignores the command when the count is larger than the
+# number of instances of the sought character.
 class ViReverseFindInLineInclusive(sublime_plugin.TextCommand):
     def run(self, edit, extend=False, character=None, _internal_mode=None, count=1):
         def f(view, s):
@@ -97,26 +100,35 @@ class ViFindInLineExclusive(sublime_plugin.TextCommand):
     """Contrary to *f*, *t* does not look past the caret's position, so if ``character`` is under
        the caret, nothing happens.
     """
-    def run(self, edit, extend=False, character=None, _internal_mode=None):
+    def run(self, edit, extend=False, character=None, _internal_mode=None, count=1):
         def f(view, s):
-            line_text = view.substr(sublime.Region(s.b, view.line(s.b).b))
-            offset = s.b - view.line(s.b).a
+            offset = s.b + 1 - view.line(s.b).a
+            a, eol = s.b + 1, view.line(s.b).b
+            final_offset = -1
 
             try:
-                where = line_text.index(character)
+                for i in range(count):
+                    line_text = view.substr(sublime.Region(a, eol))
+                    match_in_line = line_text.index(character)
+
+                    final_offset = offset + match_in_line
+
+                    a = view.line(s.a).a + final_offset + 1
+                    offset += match_in_line + 1
             except ValueError:
-                where = -1
+                pass
 
-            if where > -1:
-                pt = (view.line(s.b).a + where + offset)
+            if final_offset > -1:
+                pt = view.line(s.b).a + final_offset
 
-                state = VintageState(view)
                 # TODO: Handling modes via global state seems to be the right thing to do. Perhaps
                 # _MODE_INTERNAL_VISUAL could become a first-class mode. We know anyway that it's
                 # equivalent to MODE_NORMAL, so the necessary post processing can alway be done
                 # to ensure that we end up in MODE_NORMAL after the command.
+                state = VintageState(view)
                 if state.mode == MODE_VISUAL or _internal_mode == _MODE_INTERNAL_VISUAL:
                     return sublime.Region(s.a, pt)
+
                 return sublime.Region(pt - 1, pt - 1)
 
             return s
@@ -128,28 +140,31 @@ class ViReverseFindInLineExclusive(sublime_plugin.TextCommand):
     """Contrary to *F*, *T* does not look past the caret's position, so if ``character`` is right
        before the caret, nothing happens.
     """
-    def run(self, edit, extend=False, character=None, _internal_mode=None):
+    def run(self, edit, extend=False, character=None, _internal_mode=None, count=1):
         def f(view, s):
             line_text = view.substr(sublime.Region(view.line(s.b).a, s.b))
-            offset = 0
+            a, b = view.line(s.b).a, s.b
+            final_offset = -1
 
             try:
-                # *T* must leave the caret on the RHS of the found character.
-                where = line_text.index(character) + 1
-            except ValueError:
-                where = -1
+                for i in range(count):
+                    line_text = view.substr(sublime.Region(a, b))
+                    match_in_line = line_text.rindex(character)
 
-            if where > -1:
-                pt = view.line(s.b).a + where
+                    final_offset = match_in_line
+
+                    b = view.line(s.a).a + final_offset
+            except ValueError:
+                pass
+
+            if final_offset > -1:
+                pt = view.line(s.b).a + final_offset
 
                 state = VintageState(view)
-                # TODO: Handling modes via global state seems to be the right thing to do. Perhaps
-                # _MODE_INTERNAL_VISUAL could become a first-class mode. We know anyway that it's
-                # equivalent to MODE_NORMAL, so the necessary post processing can alway be done
-                # to ensure that we end up in MODE_NORMAL after the command.
                 if state.mode == MODE_VISUAL or _internal_mode == _MODE_INTERNAL_VISUAL:
-                    return sublime.Region(s.a, pt)
-                return sublime.Region(pt, pt)
+                    return sublime.Region(s.a, pt + 1)
+
+                return sublime.Region(pt + 1, pt + 1)
 
             return s
 
