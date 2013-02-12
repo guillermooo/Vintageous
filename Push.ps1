07bec26892273e301056940a673026edd8dfa9ec
guillermooo <guillermo.lopez@outlook.com>
2013-02-13 00:14:56 +0100

    edit script

--- Push.ps1
+++ Push.ps1
@@ -2,10 +2,10 @@
 #
 # hg-git has bug that will break the local repository if we do all the following things:
 #	1. we push changes in A.patch to Github without finalizing the patch first
-#   2. we make further changes to the patch locally and refresh
-#   3. we finalize and push A.patch to Github again
+#	2. we make further changes to the patch locally and refresh
+#	3. we finalize and push A.patch to Github again
 
-if (& hg qser) {
+if (& hg qseries) {
 	write-host "Cannot push: unfinalized patches!" -foregroundcolor RED
 	exit
 }
