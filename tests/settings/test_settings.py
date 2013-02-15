ca7e9a71b89db8b5e5b784d1092c2630cdf8eeff
guillermooo <guillermo.lopez@outlook.com>
2013-02-15 08:18:51 +0100

    improve tests

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,14 +1,37 @@
 import unittest
 
 from Vintageous.test_runner import TestsState
-
-from Vintageous.vi.settings import SublimeSettings
+from Vintageous.vi.settings import SublimeSettings, VintageSettings
 
 
 class TestSublimeSettings(unittest.TestCase):
 	def setUp(self):
 		TestsState.view.settings().erase('foo')
 		self.setts = SublimeSettings(view=TestsState.view)
+
+	def testCanInitializeClass(self):
+		self.assertEqual(self.setts.view, TestsState.view)
+
+	def testCanSetSetting(self):
+		self.assertEqual(TestsState.view.settings().get('foo'), None)
+		self.assertEqual(self.setts['foo'], None)
+
+		self.setts['foo'] = 100
+		self.assertEqual(TestsState.view.settings().get('foo'), 100)
+
+	def testCanGetSetting(self):
+		self.setts = SublimeSettings(view=TestsState.view)
+		self.setts['foo'] = 100
+		self.assertEqual(self.setts['foo'], 100)
+
+	def testCanGetNonexistingKey(self):
+		self.assertEqual(self.setts['foo'], None)
+
+
+class VintageSettings(unittest.TestCase):
+	def setUp(self):
+		TestsState.view.settings().erase('foo')
+		self.setts = VintageSettings(view=TestsState.view)
 
 	def testCanInitializeClass(self):
 		self.assertEqual(self.setts.view, TestsState.view)
