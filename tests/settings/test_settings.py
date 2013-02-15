5252740cad71e54d648de5eae259e33947a99081
guillermooo <guillermo.lopez@outlook.com>
2013-02-15 08:19:16 +0100

    fix tests

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -20,7 +20,6 @@ class TestSublimeSettings(unittest.TestCase):
 		self.assertEqual(TestsState.view.settings().get('foo'), 100)
 
 	def testCanGetSetting(self):
-		self.setts = SublimeSettings(view=TestsState.view)
 		self.setts['foo'] = 100
 		self.assertEqual(self.setts['foo'], 100)
 
@@ -28,25 +27,25 @@ class TestSublimeSettings(unittest.TestCase):
 		self.assertEqual(self.setts['foo'], None)
 
 
-class VintageSettings(unittest.TestCase):
+class TestVintageSettings(unittest.TestCase):
 	def setUp(self):
-		TestsState.view.settings().erase('foo')
+		TestsState.view.settings().erase('vintage')
 		self.setts = VintageSettings(view=TestsState.view)
 
 	def testCanInitializeClass(self):
 		self.assertEqual(self.setts.view, TestsState.view)
+		self.assertEqual(TestsState.view.settings().get('vintage'), {})
 
 	def testCanSetSetting(self):
-		self.assertEqual(TestsState.view.settings().get('foo'), None)
 		self.assertEqual(self.setts['foo'], None)
 
 		self.setts['foo'] = 100
-		self.assertEqual(TestsState.view.settings().get('foo'), 100)
+		self.assertEqual(TestsState.view.settings().get('vintage')['foo'], 100)
 
 	def testCanGetSetting(self):
-		self.setts = SublimeSettings(view=TestsState.view)
 		self.setts['foo'] = 100
 		self.assertEqual(self.setts['foo'], 100)
 
 	def testCanGetNonexistingKey(self):
 		self.assertEqual(self.setts['foo'], None)
+		
