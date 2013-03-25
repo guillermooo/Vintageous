48e3f01e30234ed5a0dc8f2f0d069d90237fda33
guillermooo <guillermo.lopez@outlook.com>
2013-03-25 07:27:39 +0100

    #164 - enable putting marks on untitled buffers

--- vi/marks.py
+++ vi/marks.py
@@ -21,16 +21,16 @@ class Marks(object):
     def get_as_encoded_address(self, name):
         win, view, rowcol = _MARKS.get(name, (None,) * 3)
         if win:
-            rowcol_part = ':'.join(str(i) for i in rowcol)
+            rowcol_encoded = ':'.join(str(i) for i in rowcol)
             fname = view.file_name()
             # TODO: Support unnamed files. Perhaps we can extend sublime text's encoded protocol to
             # target unnamed views by id: ?999:10:20
             if not fname:
-                return None
+                return "<untitled {0}>:{1}".format(view.buffer_id(), rowcol_encoded)
 
             # Marks set in the same view as the current one are returned as regions. Marks in other
             # views are returned as encoded addresses that Sublime Text understands.
             if view and view.view_id == self.state.view.view_id:
                 return sublime.Region(view.text_point(*rowcol))
             else:
-                return "{0}:{1}".format(fname, rowcol_part)
+                return "{0}:{1}".format(fname, rowcol_encoded)
