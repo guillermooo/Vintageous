0e31221ee0d85bf12dd2a7b649f894c737ae1478
guillermooo <guillermo.lopez@outlook.com>
2013-03-25 07:39:45 +0100

    #164 - 'reopen' unnamed buffers only when they are not the current buffer

--- vi/marks.py
+++ vi/marks.py
@@ -23,14 +23,15 @@ class Marks(object):
         if win:
             rowcol_encoded = ':'.join(str(i) for i in rowcol)
             fname = view.file_name()
-            # TODO: Support unnamed files. Perhaps we can extend sublime text's encoded protocol to
-            # target unnamed views by id: ?999:10:20
-            if not fname:
-                return "<untitled {0}>:{1}".format(view.buffer_id(), rowcol_encoded)
 
             # Marks set in the same view as the current one are returned as regions. Marks in other
             # views are returned as encoded addresses that Sublime Text understands.
             if view and view.view_id == self.state.view.view_id:
                 return sublime.Region(view.text_point(*rowcol))
             else:
-                return "{0}:{1}".format(fname, rowcol_encoded)
+                # FIXME: Remove buffers when they are closed.
+                if fname:
+                    return "{0}:{1}".format(fname, rowcol_encoded)
+                else:
+                    return "<untitled {0}>:{1}".format(view.buffer_id(), rowcol_encoded)
+
