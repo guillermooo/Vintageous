0160882013ec338c36cbcf42fb613250af418cdc
guillermooo <guillermo.lopez@outlook.com>
2013-05-11 23:37:26 +0200

    add 'reverse search by pt' func that does not take the whole line into account when there's a match

--- vi/search.py
+++ vi/search.py
@@ -92,3 +92,37 @@ def reverse_search(view, term, start, end, flags=0):
         if lo_line == hi_line:
             # we found the line we were looking for, now extract the match.
             return find_last_in_range(view, term, hi_line.a, min(hi_line.b, end), flags)
+
+
+def reverse_search_by_pt(view, term, start, end, flags=0):
+    assert isinstance(start, int) or start is None
+    assert isinstance(end, int) or end is None
+
+    start = start if (start is not None) else 0
+    end = end if (end is not None) else view.size()
+
+    if start < 0 or end > view.size():
+        return None
+
+    lo_line = view.full_line(start)
+    hi_line = view.full_line(end)
+
+    while True:
+        low_row, hi_row = view.rowcol(lo_line.a)[0], view.rowcol(hi_line.a)[0]
+        middle_row = (low_row + hi_row) // 2
+
+        middle_line = view.full_line(view.text_point(middle_row, 0))
+
+        lo_region = sublime.Region(lo_line.a, middle_line.b)
+        hi_region = sublime.Region(middle_line.b, min(hi_line.b, end))
+
+        if find_in_range(view, term, hi_region.a, hi_region.b, flags):
+            lo_line = view.full_line(middle_line.b)
+        elif find_in_range(view, term, lo_region.a, lo_region.b, flags):
+            hi_line = view.full_line(middle_line.a)
+        else:
+            return None
+
+        if lo_line == hi_line:
+            # we found the line we were looking for, now extract the match.
+            return find_last_in_range(view, term, max(hi_line.a, start), min(hi_line.b, end), flags)
