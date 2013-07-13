a567198ed2a28b5b064c3333d29136fa557045cf
guillermooo <guillermo.lopez@outlook.com>
2013-07-13 10:11:41 +0200

    add tests

--- tests/commands/test_set_action.py
+++ tests/commands/test_set_action.py
@@ -1,19 +1,27 @@
 import unittest
 
 from Vintageous.test_runner import TestsState
+from Vintageous.vi.constants import _MODE_INTERNAL_NORMAL
 from Vintageous.vi.constants import MODE_NORMAL
 from Vintageous.vi.constants import MODE_VISUAL
 
 import sublime
 
 
-def set_buffer_text(view, text):
+def set_text(view, text):
     view.run_command('write_to_buffer', {'text': text, 'file_name': view.file_name()})
 
 
-def pos(a=0, b=0):
-    TestsState.view.sel().clear()
-    TestsState.view.sel().add(sublime.Region(a, b))
+def add_selection(view, a=0, b=0):
+    view.sel().add(sublime.Region(a, b))
+
+
+def get_sel(view, num):
+    return view.sel()[num]
+
+
+def first_sel(view):
+    return get_sel(view, 0)
 
 
 class TestSetAction(unittest.TestCase):
@@ -38,55 +46,74 @@ class TestSetMotion(unittest.TestCase):
         self.assertEqual('vi_l', actual)
 
 
-class Test_vi_l(unittest.TestCase):
+class BufferTest(unittest.TestCase):
     def setUp(self):
         TestsState.reset_view_state()
+        self.view = TestsState.view
+        self.view.sel().clear()
+        self.R = sublime.Region
 
+
+class Test_vi_l(BufferTest):
     def testCanMoveInNormalMode(self):
-        set_buffer_text(TestsState.view, 'abc')
-        pos()
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
 
-        TestsState.view.run_command('_vi_l_motion', {'mode': MODE_NORMAL, 'count': 1, 'extend': False})
-        self.assertEqual(sublime.Region(1, 1), TestsState.view.sel()[0])
+        self.view.run_command('_vi_l_motion', {'mode': MODE_NORMAL, 'count': 1, 'extend': False})
+        self.assertEqual(self.R(1, 1), first_sel(self.view))
+
+    def testCanMoveInInternalNormalMode(self):
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
+
+        self.view.run_command('_vi_l_motion', {'mode': _MODE_INTERNAL_NORMAL, 'count': 1, 'extend': False})
+        self.assertEqual(self.R(0, 1), self.view.sel()[0])
 
     def testCanMoveInNormalModeWithCount(self):
-        set_buffer_text(TestsState.view, 'foo bar baz')
-        pos()
+        set_text(self.view, 'foo bar baz')
+        add_selection(self.view, a=0, b=0)
 
-        TestsState.view.run_command('_vi_l_motion', {'mode': MODE_NORMAL, 'count': 10, 'extend': False})
-        self.assertEqual(sublime.Region(10, 10), TestsState.view.sel()[0])
+        self.view.run_command('_vi_l_motion', {'mode': MODE_NORMAL, 'count': 10, 'extend': False})
+        self.assertEqual(self.R(10, 10), self.view.sel()[0])
+
+    def testCanMoveInInternalNormalModeWithCount(self):
+        set_text(self.view, 'foo bar baz')
+        add_selection(self.view, a=0, b=0)
+
+        self.view.run_command('_vi_l_motion', {'mode': _MODE_INTERNAL_NORMAL, 'count': 10, 'extend': False})
+        self.assertEqual(self.R(0, 10), self.view.sel()[0])
 
     def testCanMoveInVisualMode(self):
-        set_buffer_text(TestsState.view, 'abc')
-        pos()
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=1)
 
-        # TODO: Encapsulate this.
-        sel_start = TestsState.view.sel()[0].b + 1
-        TestsState.view.sel().clear()
-        TestsState.view.sel().add(sublime.Region(sel_start - 1, sel_start))
+        self.view.run_command('_vi_l_motion', {'mode': MODE_VISUAL, 'count': 1, 'extend': False})
+        self.assertEqual(self.R(0, 2), first_sel(self.view))
 
-        TestsState.view.run_command('_vi_l_motion', {'mode': MODE_VISUAL, 'count': 1, 'extend': False})
-        self.assertEqual(sublime.Region(sel_start - 1, sel_start + 1), TestsState.view.sel()[0])
+    def testCanMoveInVisualModeWithCount(self):
+        set_text(self.view, 'foo bar fuzz buzz')
+        add_selection(self.view, a=0, b=1)
+
+        self.view.run_command('_vi_l_motion', {'mode': MODE_VISUAL, 'count': 10, 'extend': False})
+        self.assertEqual(self.R(0, 11), first_sel(self.view))
 
     def testStopsAtRightEndInNormalMode(self):
-        set_buffer_text(TestsState.view, 'abc')
-        pos()
-        end = TestsState.view.line(TestsState.view.sel()[0].b).b - 1
-        expected = end
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
 
-        TestsState.view.run_command('_vi_l_motion', {'mode': MODE_NORMAL, 'count': 10000, 'extend': False})
-        self.assertEqual(TestsState.view.sel()[0], sublime.Region(2, 2))
+        self.view.run_command('_vi_l_motion', {'mode': MODE_NORMAL, 'count': 10000, 'extend': False})
+        self.assertEqual(self.R(2, 2), first_sel(self.view))
 
     def testStopsAtRightEndInVisualMode(self):
-        set_buffer_text(TestsState.view, 'abc\n')
-        pos()
-        end = TestsState.view.line(TestsState.view.sel()[0].b).b - 1
-        expected = end
+        set_text(self.view, 'abc\n')
+        add_selection(self.view, a=0, b=1)
 
-        # TODO: Encapsulate this.
-        sel_start = TestsState.view.sel()[0].b + 1
-        TestsState.view.sel().clear()
-        TestsState.view.sel().add(sublime.Region(sel_start - 1, sel_start))
+        self.view.run_command('_vi_l_motion', {'mode': MODE_VISUAL, 'count': 10000, 'extend': False})
+        self.assertEqual(self.R(0, 4), first_sel(self.view))
 
-        TestsState.view.run_command('_vi_l_motion', {'mode': MODE_VISUAL, 'count': 10000, 'extend': False})
-        self.assertEqual(TestsState.view.sel()[0], sublime.Region(0, 4))
+    def testStopsAtRightEndInInternalNormalMode(self):
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
+
+        self.view.run_command('_vi_l_motion', {'mode': _MODE_INTERNAL_NORMAL, 'count': 10000, 'extend': False})
+        self.assertEqual(self.R(0, 3), first_sel(self.view))
