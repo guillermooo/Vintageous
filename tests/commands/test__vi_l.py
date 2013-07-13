5546f9f3717ccaac55fa11993a7c96053be2be4a
guillermooo <guillermo.lopez@outlook.com>
2013-07-13 22:33:10 +0200

    tidy up

--- tests/commands/test__vi_l.py
+++ tests/commands/test__vi_l.py
@@ -11,20 +11,13 @@ from Vintageous.tests.commands import first_sel
 from Vintageous.tests.commands import BufferTest
 
 
-class Test_vi_l(BufferTest):
+class Test_vi_l_InNormalMode(BufferTest):
     def testCanMoveInNormalMode(self):
         set_text(self.view, 'abc')
         add_selection(self.view, a=0, b=0)
 
         self.view.run_command('_vi_l', {'mode': MODE_NORMAL, 'count': 1})
         self.assertEqual(self.R(1, 1), first_sel(self.view))
-
-    def testCanMoveInInternalNormalMode(self):
-        set_text(self.view, 'abc')
-        add_selection(self.view, a=0, b=0)
-
-        self.view.run_command('_vi_l', {'mode': _MODE_INTERNAL_NORMAL, 'count': 1})
-        self.assertEqual(self.R(0, 1), first_sel(self.view))
 
     def testCanMoveInNormalModeWithCount(self):
         set_text(self.view, 'foo bar baz')
@@ -33,6 +26,22 @@ class Test_vi_l(BufferTest):
         self.view.run_command('_vi_l', {'mode': MODE_NORMAL, 'count': 10})
         self.assertEqual(self.R(10, 10), first_sel(self.view))
 
+    def testStopsAtRightEndInNormalMode(self):
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
+
+        self.view.run_command('_vi_l', {'mode': MODE_NORMAL, 'count': 10000})
+        self.assertEqual(self.R(2, 2), first_sel(self.view))
+
+
+class Test_vi_l_InInternalNormalMode(BufferTest):
+    def testCanMoveInInternalNormalMode(self):
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
+
+        self.view.run_command('_vi_l', {'mode': _MODE_INTERNAL_NORMAL, 'count': 1})
+        self.assertEqual(self.R(0, 1), first_sel(self.view))
+
     def testCanMoveInInternalNormalModeWithCount(self):
         set_text(self.view, 'foo bar baz')
         add_selection(self.view, a=0, b=0)
@@ -40,6 +49,15 @@ class Test_vi_l(BufferTest):
         self.view.run_command('_vi_l', {'mode': _MODE_INTERNAL_NORMAL, 'count': 10})
         self.assertEqual(self.R(0, 10), first_sel(self.view))
 
+    def testStopsAtRightEndInInternalNormalMode(self):
+        set_text(self.view, 'abc')
+        add_selection(self.view, a=0, b=0)
+
+        self.view.run_command('_vi_l', {'mode': _MODE_INTERNAL_NORMAL, 'count': 10000})
+        self.assertEqual(self.R(0, 3), first_sel(self.view))
+
+
+class Test_vi_l_InInternalVisualMode(BufferTest):
     def testCanMoveInVisualMode(self):
         set_text(self.view, 'abc')
         add_selection(self.view, a=0, b=1)
@@ -54,23 +72,9 @@ class Test_vi_l(BufferTest):
         self.view.run_command('_vi_l', {'mode': MODE_VISUAL, 'count': 10})
         self.assertEqual(self.R(0, 11), first_sel(self.view))
 
-    def testStopsAtRightEndInNormalMode(self):
-        set_text(self.view, 'abc')
-        add_selection(self.view, a=0, b=0)
-
-        self.view.run_command('_vi_l', {'mode': MODE_NORMAL, 'count': 10000})
-        self.assertEqual(self.R(2, 2), first_sel(self.view))
-
     def testStopsAtRightEndInVisualMode(self):
         set_text(self.view, 'abc\n')
         add_selection(self.view, a=0, b=1)
 
         self.view.run_command('_vi_l', {'mode': MODE_VISUAL, 'count': 10000})
         self.assertEqual(self.R(0, 4), first_sel(self.view))
-
-    def testStopsAtRightEndInInternalNormalMode(self):
-        set_text(self.view, 'abc')
-        add_selection(self.view, a=0, b=0)
-
-        self.view.run_command('_vi_l', {'mode': _MODE_INTERNAL_NORMAL, 'count': 10000})
-        self.assertEqual(self.R(0, 3), first_sel(self.view))
