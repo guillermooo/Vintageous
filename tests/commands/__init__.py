416dc1afc79e243d19b3f1a0981789833c624330
guillermooo <guillermo.lopez@outlook.com>
2013-07-14 19:06:13 +0200

    improve helpers

--- tests/commands/__init__.py
+++ tests/commands/__init__.py
@@ -9,12 +9,27 @@ class BufferTest(unittest.TestCase):
         TestsState.reset_view_state()
         self.view = TestsState.view
         self.view.sel().clear()
-        self.R = sublime.Region
+
+    def R(self, a, b):
+        return make_region(self.view, a, b)
 
 
 def make_region_at_row(view, row=0, col=0, size=0):
     pt = view.text_point(row, col)
     return sublime.Region(pt, pt + size)
+
+
+def make_region(view, a, b):
+    try:
+        pt_a = view.text_point(*a)
+        pt_b = view.text_point(*b)
+        return sublime.Region(pt_a, pt_b)
+    except TypeError, ValueError:
+        pass
+
+    if (isinstance(a, int) and isinstance(b, int)):
+        return sublime.Region(a, b)
+    raise ValueError("a and b parameters must be either ints or (row, col)")
 
 
 def set_text(view, text):
