b9338129b3d83f9ec4e70c8892415116ad3447b1
guillermooo <guillermo.lopez@outlook.com>
2013-08-10 09:46:17 +0200

    add ctrl+k key binding for cmdline

--- ex_motions.py
+++ ex_motions.py
@@ -5,3 +5,11 @@ class _vi_cmd_line_a(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.sel().clear()
         self.view.sel().add(sublime.Region(1))
+
+
+class _vi_cmd_line_k(sublime_plugin.TextCommand):
+    def run(self, edit):
+        text = self.view.substr(sublime.Region(0, self.view.sel()[0].b))
+        self.view.replace(edit, sublime.Region(0, self.view.size()), text)
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(self.view.size()))
