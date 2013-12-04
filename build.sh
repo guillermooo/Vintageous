6719ce55f7f3f99604a4d1413dfc78dfe0429947
Leonard Truong <leonardtruong@gmail.com>
2013-12-04 09:45:45 -0800

    Updating build.sh to check OS type and find the right path

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 python builder.py
-cp -f ./dist/Vintageous.sublime-package ~/.config/sublime-text-3/Installed\ Packages
+case `uname` in
+  'Linux')
+    cp -f ./dist/Vintageous.sublime-package ~/.config/sublime-text-3/Installed\ Packages
+    ;;
+  'Darwin')
+    cp -f ./dist/Vintageous.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+    ;;
+esac
