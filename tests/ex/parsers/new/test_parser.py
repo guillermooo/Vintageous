9638e88af0805573b11b83bfc7e41d68553816f8
guillermooo <guillermo.lopez@outlook.com>
2015-03-11 20:19:33 +0100

    improve parser

--- tests/ex/parsers/new/test_parser.py
+++ tests/ex/parsers/new/test_parser.py
@@ -122,6 +122,23 @@ class parse_line_ref_Tests(unittest.TestCase):
         self.assertEqual(parsed.line_range.end_offset, [])
         self.assertEqual(parsed.line_range.must_recompute_start_line, False)
 
+    def test_CanParseDollarOnItsOwn(self):
+        parsed = start_parsing('$')
+        self.assertEqual(parsed.line_range.start_line, '$')
+
+    def test_CanParseDollarWithCompany(self):
+        parsed = start_parsing('0,$')
+        self.assertEqual(parsed.line_range.start_line, '0')
+        self.assertEqual(parsed.line_range.end_line, '$')
+
+    def test_FailsIfDollarOffset(self):
+        self.assertRaises(ValueError, start_parsing, '$+10')
+
+    def test_FailsIfPrecededByAnything(self):
+        self.assertRaises(ValueError, start_parsing, '.$')
+        self.assertRaises(ValueError, start_parsing, '/foo/$')
+        self.assertRaises(ValueError, start_parsing, '100$')
+
     def test_CanParseLoneComma(self):
         parsed = start_parsing(',')
         self.assertTrue(parsed.line_range.right_hand_side)
@@ -200,3 +217,54 @@ class parse_line_ref_Tests(unittest.TestCase):
 
     def test_FailsIfDotDigits(self):
         self.assertRaises(ValueError, start_parsing, '.10')
+
+
+class parse_line_ref_TokenComma_Tests(unittest.TestCase):
+    def test_CanParseLongSequence(self):
+        # Vim allows this.
+        parsed = start_parsing('1,2,3,4')
+        self.assertEqual(parsed.line_range.start_line, '3')
+        self.assertEqual(parsed.line_range.end_line, '4')
+
+
+class parse_line_ref_ParseSubstituteCommand(unittest.TestCase):
+    def test_CanParseItOnItsOwn(self):
+        parsed = start_parsing('substitute')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseAlias(self):
+        parsed = start_parsing('s')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseWithSimpleRange(self):
+        parsed = start_parsing('4s')
+        self.assertEqual(parsed.line_range.start_line, '4')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseWithStartAndEndLines(self):
+        parsed = start_parsing('4,5s')
+        self.assertEqual(parsed.line_range.start_line, '4')
+        self.assertEqual(parsed.line_range.end_line, '5')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseWithSearchForward(self):
+        parsed = start_parsing('/foo/s')
+        self.assertEqual(parsed.line_range.start_line, '/foo')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseWithSearchBackward(self):
+        parsed = start_parsing('?foo?s')
+        self.assertEqual(parsed.line_range.start_line, '?foo')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseWithSearchBackwardAndSearchForward(self):
+        parsed = start_parsing('?foo?,/bar/s')
+        self.assertEqual(parsed.line_range.start_line, '?foo')
+        self.assertEqual(parsed.line_range.end_line, '/bar')
+        self.assertEqual(parsed.command.content, 'substitute')
+
+    def test_CanParseWithZeroAndDollar(self):
+        parsed = start_parsing('0,$s')
+        self.assertEqual(parsed.line_range.start_line, '0')
+        self.assertEqual(parsed.line_range.end_line, '$')
+        self.assertEqual(parsed.command.content, 'substitute')
