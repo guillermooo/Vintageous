4276c72ad5633a26e5b9e39f1743cc12fe92cfd5
guillermooo <guillermo.lopez@outlook.com>
2015-03-19 23:44:06 +0100

    don't serialize the parsed command line for now
    Instead of parsing, serializing into Json and passing that as
    parameters to the ST command, we simply pass commands the original
    command line string and parse it again in the command.
    
    This seems simpler than serializing.

--- ex/parsers/new/tokens.py
+++ ex/parsers/new/tokens.py
@@ -5,9 +5,6 @@ class TokenEof(Token):
     def __init__(self, *args, **kwargs):
         super().__init__(TOKEN_EOF, '__EOF__', *args, **kwargs)
 
-    def to_json(self):
-        raise TypeError('why would you serialize EOF?')
-
 
 class TokenOfRange(Token):
     pass
@@ -33,20 +30,8 @@ class TokenSemicolon(TokenOfRange):
 
 
 class TokenOffset(TokenOfRange):
-    def __init__(self, *args, **kwargs):
-        content = 0
-        if args:
-            content = args[0]
-            args = args[1:]
-
+    def __init__(self, content, *args, **kwargs):
         super().__init__(TOKEN_OFFSET, content, *args, **kwargs)
-
-    def to_json(self):
-        return {
-            'token_type': self.token_type,
-            'args': [self.content],
-            'kwargs': {}
-        }
 
 
 class TokenPercent(TokenOfRange):
@@ -60,63 +45,22 @@ class TokenDot(TokenOfRange):
 
 
 class TokenSearchForward(TokenOfSearch):
-    def __init__(self, *args, **kwargs):
-        content = None
-        if args:
-            content = args[0]
-            args = args[1:]
-
+    def __init__(self, content, *args, **kwargs):
         super().__init__(TOKEN_SEARCH_FORWARD, content, *args, **kwargs)
-
-    def to_json(self):
-        return {
-            'token_type': self.token_type,
-            'args': [] if self.content is None else [self.content],
-            'kwargs': {}
-        }
 
 
 class TokenSearchBackward(TokenOfSearch):
-    def __init__(self, *args, **kwargs):
-        content = None
-        if args:
-            content = args[0]
-            args = args[1:]
-
+    def __init__(self, content, *args, **kwargs):
         super().__init__(TOKEN_SEARCH_BACKWARD, content, *args, **kwargs)
-
-    def to_json(self):
-        return {
-            'token_type': self.token_type,
-            'args': [] if self.content is None else [self.content],
-            'kwargs': {}
-        }
 
 
 class TokenDigits(TokenOfRange):
-    def __init__(self, *args, **kwargs):
-        content = None
-        if args:
-            content = args[0]
-            args = args[1:]
-
+    def __init__(self, content, *args, **kwargs):
         super().__init__(TOKEN_DIGITS, content, *args, **kwargs)
-
-    def to_json(self):
-        return {
-            'token_type': self.token_type,
-            'args': [] if self.content is None else [self.content],
-            'kwargs': {}
-        }
 
 
 class TokenMark(TokenOfRange):
-    def __init__(self, *args, **kwargs):
-        content = None
-        if args:
-            content = args[0]
-            args = args[1:]
-
+    def __init__(self, content, *args, **kwargs):
         super().__init__(TOKEN_MARK, content, *args, **kwargs)
 
     def __str__(self):
@@ -124,13 +68,6 @@ class TokenMark(TokenOfRange):
 
     def __repr__(self):
         return "<[{0}]('{1})>".format(self.__class__.__name__, self.content)
-
-    def to_json(self):
-        return {
-            'token_type': self.token_type,
-            'args': [] if self.content is None else [self.content],
-            'kwargs': {}
-        }
 
     @property
     def exact(self):
