867616518cbce4731d49a2a3e9cc6d34e2127063
guillermooo <guillermo.lopez@outlook.com>
2015-04-04 10:27:18 +0200

    refactor

--- bin/Stats.ps1
+++ bin/Stats.ps1
@@ -7,21 +7,24 @@ push-location $PSSCriptRoot\..
 				select-string -notmatch "^\s*#" |
 					measure-object -line
 
+	write-output "SLOC (Application): $($code.lines - $tests.lines)"
+
 	$comments = get-content (
 		get-childitem . *.py -recurse |
 			select-object -expandproperty fullname) |
 				select-string "^\s*#" |
 					measure-object -line
 
+	write-output "SLOC (Comments): $($comments.lines)"
+
 	$tests = get-content (
 		get-childitem .\tests *.py -recurse |
 			select-object -expandproperty fullname) |
 				select-string -notmatch "^\s*#" |
 					measure-object -line
+					
+	write-output "SLOC (Tests): $($tests.lines)"
 pop-location
 
-write-output "SLOC (Application): $($code.lines - $tests.lines)"
-write-output "SLOC (Tests): $($tests.lines)"
-write-output "SLOC (Comments): $($comments.lines)"
 write-output "-----------------------------------------------"
 write-output "SLOC (Total): $($code.lines + $comments.lines)"
