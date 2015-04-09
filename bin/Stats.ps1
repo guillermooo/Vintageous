02a6807a56f499cae66c7c2c492ddd1f61b0f448
guillermooo <guillermo.lopez@outlook.com>
2015-04-09 19:58:53 +0200

    fix script

--- bin/Stats.ps1
+++ bin/Stats.ps1
@@ -7,7 +7,6 @@ push-location $PSSCriptRoot\..
 				select-string -notmatch "^\s*#" |
 					measure-object -line
 
-	write-output "SLOC (Application): $($code.lines - $tests.lines)"
 
 	$comments = get-content (
 		get-childitem . *.py -recurse |
@@ -15,16 +14,17 @@ push-location $PSSCriptRoot\..
 				select-string "^\s*#" |
 					measure-object -line
 
-	write-output "SLOC (Comments): $($comments.lines)"
 
 	$tests = get-content (
 		get-childitem .\tests *.py -recurse |
 			select-object -expandproperty fullname) |
 				select-string -notmatch "^\s*#" |
 					measure-object -line
-					
-	write-output "SLOC (Tests): $($tests.lines)"
+
 pop-location
 
 write-output "-----------------------------------------------"
+write-output "SLOC (Application): $($code.lines - $tests.lines)"
+write-output "SLOC (Tests): $($tests.lines)"
+write-output "SLOC (Comments): $($comments.lines)"
 write-output "SLOC (Total): $($code.lines + $comments.lines)"
